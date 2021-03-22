b2fdf90bd68917a3fdc50f6d9d0145d0aa864756
wilsonvetdev <wilsongch92@gmail.com>
2021-03-21 21:54:08 -0400

    wip

--- pig.py
+++ pig.py
@@ -1,10 +1,12 @@
 import random
 
+
 class Player:
 
-    def __init__(self, num=None, score=0):
+    def __init__(self, num=None, total_score=0, temp_scores=[]):
         self.num = num
-        self.score = score
+        self.total_score = total_score
+        self.temp_scores = temp_scores
 
 
 class Die:
@@ -21,8 +23,31 @@ class Game:
 
     def __init__(self, player_count, player_one, player_two):
         self.max_score = 100
-        self.scores = [player_one.score, player_two.score]
+        self.scores = [player_one.total_score, player_two.total_score]
         self.player_count = player_count
+        self.current_player = player_one
+
+    def toggle_player(self, player_one, player_two):
+        if self.current_player == player_one:
+            self.current_player = player_two
+            return self.current_player
+
+        if self.current_player == player_two:
+            self.current_player = player_one
+            return self.current_player
+    
+    def start_game(self, player_one, player_two):
+        while max(self.scores) < self.max_score:
+            rolling = input(f'Player {self.current_player.num} rolling? Press "r" to roll. -->')
+
+            if rolling:
+                current_roll = game_dice.roll()
+                print(f'Player {self.current_player.num} rolled a {current_roll}.')
+
+                if current_roll == 1:
+                    print(f'That\'s too bad! Your score returns to 0.')
+                    self.toggle_player(player_one, player_two) # can't toggle while inside the start_game function
+                    print(self.current_player.num)
 
 
 if __name__ == '__main__':
@@ -32,11 +57,4 @@ if __name__ == '__main__':
     player_two = Player(num=2)
     game = Game(2, player_one, player_two)
 
-
-    while max(game.scores) < game.max_score:
-        rolling = input(f'Player {player_one.num} rolling? Press "r" to roll. -->')
-
-        if rolling:
-            current_roll = game_dice.roll()
-            print(f'Player {player_one.num} rolled a {current_roll}.')
-            
+    game.start_game(player_one, player_two)
